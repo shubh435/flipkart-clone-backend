68899590706837225cd36d771a1c5156604d13da
shubham <intern.shubham@sangnak.in>
2022-04-23 15:46:40 +0530

    created the category

---
+++ src/models/category.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    parentId: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
